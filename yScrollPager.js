ebf6878f5a7418caf7794b9cd13746f9b33ccb2a
Amit Gupta <amitg@aditi.com>
2015-11-09 17:53:23 +0530

    Implements 1) Next and Prev buttons 2) page number in dropdown

--- yScrollPager.js
+++ yScrollPager.js
@@ -6,10 +6,26 @@ function smoothScrollTo(ele) {
         scrollTop: $(ele).offset().top
     }, 600);
 }
+function yScrollPagerPrev() {
+    yScrollPagerMove(-1)
+}
+
+function yScrollPagerNext() {
+    yScrollPagerMove(1)
+}
+function yScrollPagerMove(val) {
+    console.log($("#yScrollPagerNav").attr("data-current-section"))
+    var nextEleIndex = parseInt(parseInt($("#yScrollPagerNav").attr("data-current-section")) + parseInt(val))
+    var nextEle = $(".yScrollPage").eq(nextEleIndex)
+    smoothScrollTo(nextEle)
+    $("#yScrollPagerNav").attr("data-current-section", nextEleIndex)
+}
 $(document).scroll(function() {
     $.each($(".yScrollPage"), function (index, ele) {
         if (($(ele).offset().top - $("#yScrollPagerNav").height() <= $(document).scrollTop()) && ($(".yScrollPage").eq(index + 1).offset().top - $("#yScrollPagerNav").height() > $(document).scrollTop())) {            
             $("#yScrollPagerNav>span.pull-left").text($(ele).attr("data-title"))
+            $("#dropdownMenu2").text(index+1 + " of " + $(".yScrollPage").length)
+            $("#yScrollPagerNav").attr("data-current-section", index)
         }
     })
 })
